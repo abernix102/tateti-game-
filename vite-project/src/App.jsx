20aa2d024e4b30c49d41bdca4e5e25b37022fabd
abernix102 <nahuelo841@gmail.com>
2023-10-20 09:18:52 -0300

    tateti game

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,137 @@
+import { useState } from "react";
+
+const TURNS = {
+  X: "x",
+  O: "o"
+};
+
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
+const Square = ({ children,isSelected, updateBoard, index }) => {
+  const className = `square  ${isSelected ? 'is-selected' : ''}`
+
+  const handleClick = () => {
+    updateBoard(index);
+  };
+
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  );
+};
+
+const App = () => {
+  const [score, setScore] = useState(
+    {
+      player1: 0,
+      player2: 0
+    }
+  );
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [turn, setTurn] = useState(TURNS.X);
+  const [winner, setWinner] = useState(null)
+
+  const updateBoard = (index) => {
+    if(board[index] || winner )return
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn)
+    const newWinner = winnerTurn(newBoard)
+    if (newWinner) {
+      if (newWinner === TURNS.X) {
+        setScore(prevScore => ({
+          ...prevScore,
+          player1: prevScore.player1 + 1
+        }))
+      }else if(newWinner === TURNS.O){
+        setScore(prevScore => ({
+          ...prevScore,
+          player2: prevScore.player2 + 1
+        }))
+      }
+      setWinner(newWinner);
+    }
+    
+  };
+
+  const winnerTurn = (boardcheck) => {
+    for (const  combo of WINNER_COMBOS) {
+        const [a,b,c] = combo;
+        if(boardcheck[a] && boardcheck[a] === boardcheck[b] && boardcheck[a]=== boardcheck[c]){
+            return boardcheck[a]
+        }
+    }
+    if(winner) return null
+  }
+  
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.X)
+    setWinner(null)
+  }
+
+  return (
+    <main className="board">
+      <h1>Mi Tateti pa</h1>
+      <section>
+        <h1>Player X : {score.player1}</h1>
+        <h1>Player O : {score.player2}</h1>
+      </section>
+      <section className="game">
+        {board.map((_, index) => {
+          return (
+            <Square 
+            key={index} 
+            index={index} 
+            updateBoard={updateBoard}>
+              {board[index]}
+            </Square>
+          );
+        })}
+      </section>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X }>{TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
+      </section>
+      {
+        winner !==  null && (
+          <section className="winner">
+            <div className="text">
+              <h2>
+                {
+                  winner  === false ? 'Empate' : 'ganaste hijo de puta ganaste'
+                }
+                <header className="win">
+                  {winner && <Square>{winner}</Square>}
+                </header>
+                <button onClick={resetGame} className="win">
+                  reset
+                </button>
+                <button onClick={resetGame}>
+                  reset game
+                </button>
+              </h2>
+            </div>
+          </section>
+        )
+      }
+    </main>
+  );
+};
+
+export default App;
